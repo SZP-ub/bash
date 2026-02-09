f1d3a5a490981ca4c3958d0cd981c8c0377cac90
SZP-ub <SZP0629@outlook.com>
2026-02-09 23:02:04 +0800

    tex

--- .config/nvim/lua/plugins/tex.lua
+++ .config/nvim/lua/plugins/tex.lua
@@ -9,6 +9,30 @@ return {
 			vim.g.tex_flavor = "latex"
 
 			vim.g.vimtex_compiler_method = "latexmk"
+
+			-- VimTeX 配置
+			vim.g.vimtex_syntax_conceal_disable = 1 -- 禁用 VimTeX 隐藏
+
+			-- 全局禁用 conceallevel
+			vim.opt.conceallevel = 0
+
+			-- 或者使用细致控制
+			vim.g.vimtex_syntax_conceal = {
+				accents = 0,
+				ligatures = 0,
+				cites = 0,
+				fancy = 0,
+				spacing = 0,
+				greek = 0,
+				math_bounds = 0,
+				math_delimiters = 0,
+				math_fracs = 0,
+				math_super_sub = 0,
+				math_symbols = 0,
+				sections = 0,
+				styles = 0,
+			}
+
 			-- vim.g.vimtex_compiler_autostart = 1
 			-- vim.g.vimtex_compiler_start_on_save = 1
 			vim.g.vimtex_compiler_latexmk = {
